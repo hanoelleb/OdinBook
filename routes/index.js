d8d7182acc98eb94678cd8b4a5f95e0841a26747
hanoelleb <hanoelleb@gmail.com>
2020-07-14 15:30:40 -0700

    set up routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,39 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* Profile */
+
+router.get('/profile/:id/', function(req, res, next) {
+  res.send('NOT IMPLEMENTED: get profile page');
+})
+
+router.get('/profile/:id/edit', function(req, res, next) {
+  res.send('NOT IMPLEMENTED: get edit profile page');
+})
+
+router.post('/profile/:id/edit', function(req, res, next) {
+  res.send('NOT IMPLEMENTED: post edit profile page');
+})
+
+router.get('/profile/browse/:id/posts/'), function(req, res, next) {
+  res.send('NOT IMPLEMENTED: get other user\'s profile page');
+})
+
+router.get('/profile/browse/:id/posts/:pid'), function(req, res, next) {
+  res.send('NOT IMPLEMENTED: get other user post page');
+})
+
+/* Comment */
+
+router.get('/profile/browse/:id/posts/:pid/comment'), 
+  function(req, res, next) {
+     res.send('NOT IMPLEMENTED: get comment to post');
+})
+
+router.post('/profile/browse/:id/posts/:pid/comment'),
+  function(req, res, next) {
+     res.send('NOT IMPLEMENTED: post comment to post');
+})
+
+
 module.exports = router;
